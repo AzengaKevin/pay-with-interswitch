69cfaf6c6c4f447662905faa6591ac5bfeb0ed2f
Azenga Kevin <toomuch573@gmail.com>
2022-11-02 21:33:25 +0300

    WIP: Updated README.md and some general changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1 class="fw-bold display-3">{{ config('app.name') }}</h1>
+                    <h1 class="fw-bold display-3 text-primary">{{ config('app.name') }}</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
